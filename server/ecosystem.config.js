64518ee619ad977d7b9847a29983d18711b199f4
tekyu <patryyk21@gmail.com>
2021-02-17 22:37:43 +0100

    Server ecosystem for pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,16 +1,25 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'Deckit Front',
+//       script: 'serve',
+
+//       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+//       env: {
+//         PM2_SERVE_PATH: '.',
+//         PM2_SERVE_PORT: '4314',
+//         PM2_SERVE_SPA: 'true',
+//         PM2_SERVE_HOMEPATE: './index.html',
+//       },
+//     },
+//   ],
+// };
 module.exports = {
   apps: [
     {
-      name: 'Deckit Front',
-      script: 'serve',
-
-      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      env: {
-        PM2_SERVE_PATH: '.',
-        PM2_SERVE_PORT: '4314',
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPATE: './index.html',
-      },
+      name: 'Scattegories Backend',
+      script: 'app.js',
+      node_args: '-r dotenv/config',
     },
   ],
 };
