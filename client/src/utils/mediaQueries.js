6ba881a0e1a938e08ae45e944846a665f83a0020
Patryk Rosiak <patryyk21@gmail.com>
2021-05-03 11:30:29 +0200

    Refactored media queries

--- client/src/utils/mediaQueries.js
+++ client/src/utils/mediaQueries.js
@@ -10,11 +10,4 @@ export const mediaQueries = {
   waitingScreen: `550px`
 };
 
-export const mediaQuery = generateMedia({
-  huge: `1440px`,
-  large: `1170px`,
-  medium: `768px`,
-  small: `450px`,
-  menu: `860px`,
-  waitingScreen: `550px`
-});
+export const mediaQuery = generateMedia(mediaQueries);
